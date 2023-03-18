599093e91d80c54f9c0041b08f12cedaa11e2b7c
vikkelas <bass89@mail.ru>
2023-03-18 20:03:28 +0300

    fix modal

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,25 +1,23 @@
 import style from '../styles/Modal.module.sass'
 import {NextPage} from "next";
 
-interface PropsModal {
-    children:
-}
 
-const Modal:NextPage<PropsModal> = (props) => {
 
-    return (
-        <div
-            onClick={()=>{
+const Modal= () => {
 
-            }}
-            className={modalStatus?style.modalActive:style.modal}>
-            <div
-                onClick={(e)=>e.stopPropagation()}
-                className={modalStatus?style.contentActive:style.content}>
-                {children}
-            </div>
-        </div>
-    );
+    // return (
+    //     <div
+    //         onClick={()=>{
+    //
+    //         }}
+    //         className={modalStatus?style.modalActive:style.modal}>
+    //         <div
+    //             onClick={(e)=>e.stopPropagation()}
+    //             className={modalStatus?style.contentActive:style.content}>
+    //             {children}
+    //         </div>
+    //     </div>
+    // );
 };
 
 export default Modal;
