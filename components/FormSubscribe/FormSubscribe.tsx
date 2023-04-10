b2950128838efc3a43437ae26a5d36e77d13487f
vikkelas <bass89@mail.ru>
2023-04-10 21:31:41 +0300

    add modal send mail

--- components/FormSubscribe/FormSubscribe.tsx
+++ components/FormSubscribe/FormSubscribe.tsx
@@ -5,6 +5,10 @@ import {useDispatch, useSelector} from "react-redux";
 import {changeModal, ModalState} from "@/store/reducer/modalSlice";
 
 const FormSubscribe = () => {
+    const [response, setResponse] =useState(false)
+    const [responseBody, setResponseBody] = useState<
+        any | null>(null)
+
     const [formState, setFormState] = useState({
         name: '',
         telephone: '',
@@ -21,72 +25,110 @@ const FormSubscribe = () => {
 
     const dispatch = useDispatch()
 
-    const sendForm = (event: React.MouseEvent<HTMLButtonElement>)=>{
+    const sendForm = async (event: React.MouseEvent<HTMLButtonElement>)=>{
         event.preventDefault()
-        dispatch(changeModal(false))
+        try {
+            const response = await fetch('http://localhost:3000/api/send-mail', {
+                method: "POST",
+                body: JSON.stringify(formState)
+            })
+            if(response.ok){
+                const data = await response.json()
+                setFormState({
+                    name: '',
+                    telephone: '',
+                    email: '',
+                    role: '1'
+                })
+                setResponseBody(data)
+                setResponse(true)
+            }
+        } catch (err){
+            console.log(err)
+        }
+
+        // dispatch(changeModal(false))
     }
     return (
-        <div className={styles.formInfo}>
-            <h2>Принять участие</h2>
-            <p>Оставьте свои данные и мы свяжемся с вами в ближайшее время!</p>
-            <form className={styles.formInfoForm}>
-                <label>
-                    <span>Имя</span>
-                    <input
-                        name={'name'}
-                        type="text"
-                        onChange={changeForm}
-                        value={formState.name}
-                    />
-                </label>
-                <label>
-                    <span>Телефон для связи</span>
-                    <input
-                        name={'tel'}
-                        type="text"
-                        onChange={changeForm}
-                        value={formState.telephone}
-                    />
-                </label>
-                <label>
-                    <span>email</span>
-                    <input
-                        name={'email'}
-                        type="text"
-                        onChange={changeForm}
-                        value={formState.email}
-                    />
-                </label>
-                <div className={styles.formInfoFormRadio}>
-                    <span>Укажите ваш статус</span>
+        <>
+            {!response&&<div className={styles.formInfo}>
+                <h2>Принять участие</h2>
+                <p>Оставьте свои данные и мы свяжемся с вами в ближайшее время!</p>
+                <form className={styles.formInfoForm}>
                     <label>
-                        <span>Участник</span>
+                        <span>Имя</span>
                         <input
-                            name="role"
-                            type="radio"
-                            value={'1'}
-                            checked={formState.role==='1'}
+                            required
+                            name={'name'}
+                            type="text"
                             onChange={changeForm}
+                            value={formState.name}
                         />
                     </label>
                     <label>
-                        <span>Посетитель</span>
+                        <span>Телефон для связи</span>
                         <input
-                            name="role"
-                            type="radio"
-                            value={'2'}
-                            checked={formState.role==='2'}
+                            required
+                            name={'tel'}
+                            type="text"
                             onChange={changeForm}
+                            value={formState.telephone}
                         />
                     </label>
+                    <label>
+                        <span>email</span>
+                        <input
+                            required
+                            name={'email'}
+                            type="email"
+                            onChange={changeForm}
+                            value={formState.email}
+                        />
+                    </label>
+                    <div className={styles.formInfoFormRadio}>
+                        <span>Укажите ваш статус</span>
+                        <label>
+                            <span>Участник</span>
+                            <input
+                                name="role"
+                                type="radio"
+                                value={'1'}
+                                checked={formState.role==='1'}
+                                onChange={changeForm}
+                            />
+                        </label>
+                        <label>
+                            <span>Посетитель</span>
+                            <input
+                                name="role"
+                                type="radio"
+                                value={'2'}
+                                checked={formState.role==='2'}
+                                onChange={changeForm}
+                            />
+                        </label>
+                    </div>
+                    <button
+                        onClick={sendForm}
+                    >
+                        Учавствовать
+                    </button>
+                </form>
+            </div>}
+            {response&&responseBody!==null&&
+                <div className={styles.response}>
+                    <p>{responseBody.message}</p>
+                    <button
+                        onClick={(event)=>{
+                            event.preventDefault()
+                            dispatch(changeModal(false))
+                            setResponse(false)
+                        }}
+                    >ок</button>
                 </div>
-                <button
-                    onClick={sendForm}
-                >
-                    Учавствовать
-                </button>
-            </form>
-        </div>
+            }
+        </>
+
     );
 };
 
--- pages/api/send-mail.ts
+++ pages/api/send-mail.ts
@@ -1,30 +1,58 @@
- import type { NextApiRequest, NextApiResponse } from 'next'
-//
-// interface ResponseData {
-//
-// }
-// export default function handler(
-//     req:  NextApiRequest,
-//     res: NextApiResponse<ResponseData | {error: string}
-// ) {
-//
-//     const message = {
-//         from: req.body.email,
-//         to: process.env.GMAIL_EMAIL_ADDRESS,
-//         subject: req.body.subject,
-//         text: req.body.message,
-//         html: `<p>${req.body.message}</p>`,
-//     };
-//
-//     let transporter = nodemailer.createTransport({
-//         service: 'Gmail',
-//         auth: {
-//             user: process.env.GMAIL_EMAIL_ADDRESS,
-//             pass: process.env.GMAIL_APP_PASSWORD,
-//         },
-//     });
-//
-//     if (req.method === 'POST'){
-//
-//     }
-// }
+import type { NextApiRequest, NextApiResponse } from 'next'
+const nodemailer = require('nodemailer');
+
+interface ResponseData {
+    message: string
+    ok: boolean
+}
+export default function handler(
+    req:  NextApiRequest,
+    res: NextApiResponse<ResponseData | {error: string}>
+) {
+
+    const objMessage = JSON.parse(req.body)
+    const message = {
+        from: 'ZdorovieEXPO <mail@zdorovie-expo.ru>',
+        to: 'mail@zdorovie-expo.ru',
+        subject: 'Заявка с сайта',
+        html: `
+        <body>
+            <h2>Вам отправелнна заявка с сайта ЗдоровьеEXPO</h2>
+            <br>
+            <h3>Имя</h3>
+            <p>${objMessage.name}</p> 
+            <h3>Телефон</h3>
+            <p>${objMessage.telephone}</p>        
+            <h3>Почта</h3>
+            <p>${objMessage.email}</p>      
+            <h3>В роли</h3>
+            <p>${objMessage.role==='1'?'Участника':'Посетителя'}</p>  
+        </body>`,
+    };
+
+    const transporter = nodemailer.createTransport({
+        host: "smtp.timeweb.ru",
+        port: 25,
+        auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASSWORD,
+        },
+    });
+
+    if (req.method === 'POST'){
+        transporter.sendMail(message,(err: any, info: { accepted: any; })=>{
+            if (err) {
+                console.log(err)
+                res.status(404).json({
+                    message: 'ошибка на сервере',
+                    ok: false
+                });
+            } else {
+                res.status(250).json({
+                    message: `Ваша заявка принята`,
+                    ok: true
+                });
+            }
+        })
+    }
+}
